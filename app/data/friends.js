3c090cd8b80e0de2864ad813c2e93b5ecfbfef3a
Zelaway <zelaway@me.com>
2016-05-04 15:16:09 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer. 
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+
+var friendsID = [
+	{
+		customerName: "Adam",
+		customerEmail: "zelaway@me.com",
+		customerID: "asawyer81",
+		facebookUrl: "https://www.facebook.com/adam.sawyer.73"
+		q1: "3",
+		q2: "3", 
+		q3: "3", 
+		q4: "3",
+		q5: "3",
+		q6: "3",
+		q7: "3",
+		q8: "3",
+		q9: "3",
+		q10:"3",
+
+
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = friendsID; 
